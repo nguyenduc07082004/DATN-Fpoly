3ad3031a1a5fe92cbee31eef73590eb01ab4f633
quangvt12 <quangtranvinh802@gmail.com>
2024-10-12 10:21:33 +0700

    main

--- back-end/src/models/UserModels.js
+++ back-end/src/models/UserModels.js
@@ -4,8 +4,8 @@ const mongoose= require ('mongoose');
 const userSchema= new mongoose.Schema(
 
     {
-       name : {type:String,require:true},
-       age :{type:Number,require:true},
+        name : {type:String,require:true},
+        age :{type:Number,require:true},
         address :{type :String,require:true},
         email:{type: mongoose.Schema.Types.ObjectId ,ref: 'Login', require: true},
         sdt:{type:Number,require:true},
