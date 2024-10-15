1beb1a6d7b265503f6c7bed98a0e20d9d4e5f1ef
quangvt12 <quangtranvinh802@gmail.com>
2024-10-15 10:51:34 +0700

    main

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,10 +1,9 @@
-const express = require("express");
-const dotenv = require("dotenv/config");
+const express = require("express"); 
 const mongoose = require("mongoose");
 const authRouter=require("./routes/auth");
 
 
-dotenv.config();
+require("dotenv/config");
 
 const app = express();
 const port = process.env.PORT || 3000;
