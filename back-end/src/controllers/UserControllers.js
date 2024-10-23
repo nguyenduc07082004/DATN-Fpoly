1f5c60be483f633678824674c107fe0bd006808a
quangvt12 <quangtranvinh802@gmail.com>
2024-10-23 10:57:25 +0700

    quang

--- back-end/src/controllers/UserControllers.js
+++ back-end/src/controllers/UserControllers.js
@@ -1,7 +1,6 @@
 const User = require("../models/UserModels");
 
-
-// Lấy danh sách sản phẩm
+m
 exports.getUser = async (req, res) => {
   try {
 
@@ -12,18 +11,7 @@ exports.getUser = async (req, res) => {
   }
 };
 
-// Thêm sản phẩm
-exports.addUser = async (req, res) => {
-  const newuser = new User(req.body);
-  try {
-    const saveuser = await newuser.save();
-    res.status(201).json(saveuser);
-  } catch (error) {
-    res.status(500).json({ message: "Lỗi khi thêm sản phẩm", error });
-  }
-};
 
-// Cập nhật sản phẩm
 exports.updateUser = async (req, res) => {
   try {
     const Updateuser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
@@ -33,7 +21,7 @@ exports.updateUser = async (req, res) => {
   }
 };
 
-// Xóa sản phẩm
+
 exports.deleteUser = async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
--- back-end/src/models/UserModels.js
+++ back-end/src/models/UserModels.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
+  username:{type:String,require:true},
+  email:{type:String,require:true},
+  password:{type:String,require:true},
   fullname:{ type: String, require: true },
   age: { type: Number, require: true },
   address: { type: String, require: true },
-  email: { type: mongoose.Schema.Types.ObjectId, ref: "Login", require: true },
   sdt: { type: Number, require: true },
-  role: { type: mongoose.Schema.Types.ObjectId, ref: "Login", require: true },
+  role:{type:String,default:"user"},
 });
 
 const User = mongoose.model("User", userSchema);
